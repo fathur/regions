41eb5720d89bdf9935356c2cdda408fa1010f400
Fathur <fathur@telectg.co.id>
2018-05-12 19:53:06 +0700

    api regions

---
+++ app/Http/Controllers/RegionController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fathur
+ * Date: 12/05/18
+ * Time: 17.35
+ */
+
+namespace App\Http\Controllers;
+
+
+use App\Http\Controllers\Controller;
+use App\Model\Region;
+use App\Transformers\RegionTransformer;
+use Illuminate\Http\Request;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
+
+class RegionController extends Controller
+{
+    protected $fractal;
+
+    public function __construct()
+    {
+        $this->fractal = new Manager();
+
+    }
+
+    public function lists(Request $request)
+    {
+        if ($request->has('parent')) {
+            $parent = Region::find($request->get('parent'));
+            $regions = $parent->children;
+        } else {
+            $regions = Region::whereIsRoot()->get();
+        }
+
+        $resource = new Collection($regions, new RegionTransformer());
+        $data = $this->fractal->createData($resource)->toArray();
+
+        return response()->json($data);
+    }
+
+    public function view($id, Request $request)
+    {
+        $region = Region::find($id);
+        $resource = new Item($region, new RegionTransformer());
+        $data = $this->fractal->createData($resource)->toArray();
+
+        return response()->json($data);
+    }
+}
---
+++ app/Transformers/RegionTransformer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fathur
+ * Date: 12/05/18
+ * Time: 19.45
+ */
+
+namespace App\Transformers;
+
+
+use League\Fractal\TransformerAbstract;
+
+class RegionTransformer extends TransformerAbstract
+{
+    public function transform($region)
+    {
+        return [
+            'key' => $region->id,
+            'name' => $region->name,
+            'postal_code' => $region->postal_code ?? ""
+        ];
+    }
+}
